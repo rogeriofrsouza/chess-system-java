5690bd9a148ef4c1d0d18a03e9a7bcfda13e5b40
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-02-15 13:42:14 -0300

    Placing pieces on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 // Coração do sistema de xadrez, contém as regras do jogo de xadrez
 public class ChessMatch {
@@ -9,6 +12,8 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);  // Esta classe deve saber as dimensões de um jogo de xadrez
+		
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -21,6 +26,13 @@ public class ChessMatch {
 		}
 		
 		return mat;
+	}
+	
+	// Inicia a partida de xadrez colocando as peças no tabuleiro
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 	}
 	
 }
