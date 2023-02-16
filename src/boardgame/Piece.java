d3bb185defaa27148dde39741bee62df3bd10081
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-02-15 21:29:23 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	// Não é ainda a posição do xadrez, é uma posição simples de matriz. Não deve ser visível na camada de xadrez
 	protected Position position;
@@ -18,5 +18,29 @@ public class Piece {
 	}
 
 	// Não permitir que o Board seja alterado. Remover o setBoard()
+	
+	public abstract boolean[][] possibleMoves();
+	
+	/*
+	 *  Hook method: método que faz um gancho com a subclasse.
+	 *  Chama uma possível implementação de alguma subclasse concreta da classe Piece
+	 */
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
 	
 }
