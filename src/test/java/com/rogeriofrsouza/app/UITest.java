57fc6f025f979192d4be998905963c62710e0b6e
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-07-27 00:43:54 -0300

    test(UI): add unit test to clearScreen()

---
+++ src/test/java/com/rogeriofrsouza/app/UITest.java
@@ -0,0 +1,42 @@
+package com.rogeriofrsouza.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+@ExtendWith(MockitoExtension.class)
+class UITest {
+
+    @InjectMocks private UI ui;
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    @DisplayName("should clear and flush the console")
+    void clearScreen() {
+        ui.clearScreen();
+
+        String expected = UI.ANSI_MOVE_CURSOR_HOME + UI.ANSI_CLEAR_SCREEN;
+        assertEquals(expected, outputStreamCaptor.toString());
+    }
+}
