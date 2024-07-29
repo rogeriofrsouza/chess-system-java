cc9bca6e27fe2e0337489519391a78ac2bf4f5d1
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-07-29 00:41:36 -0300

    test(UI): add unit tests for printBoard method

--- src/test/java/com/rogeriofrsouza/app/UITest.java
+++ src/test/java/com/rogeriofrsouza/app/UITest.java
@@ -5,8 +5,11 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 
+import com.rogeriofrsouza.app.boardgame.Board;
 import com.rogeriofrsouza.app.chess.ChessMatch;
 import com.rogeriofrsouza.app.chess.ChessPiece;
+import com.rogeriofrsouza.app.chess.Color;
+import com.rogeriofrsouza.app.chess.pieces.Rook;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -82,6 +85,9 @@ class UITest {
     void printMatch_isCheck_logMatchAndCheck() {
         ChessMatch chessMatch = new ChessMatch();
         chessMatch.setCheck(true);
+        chessMatch.setTurn(50);
+        chessMatch.setCurrentPlayer(Color.BLACK);
+
         List<ChessPiece> captured = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder();
 
@@ -112,6 +118,9 @@ class UITest {
     void printMatch_isCheckMate_logMatchAndCheckMate() {
         ChessMatch chessMatch = new ChessMatch();
         chessMatch.setCheckMate(true);
+        chessMatch.setTurn(10);
+        chessMatch.setCurrentPlayer(Color.BLACK);
+
         List<ChessPiece> captured = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder();
 
@@ -134,5 +143,57 @@ class UITest {
 
         assertEquals(stringBuilder.toString(), outputStreamCaptor.toString());
         verify(ui).printBoard(any(ChessPiece[][].class), any());
+    }
+
+    @Test
+    @DisplayName("should print the board, pieces and its corresponding colors")
+    void printBoard_noPossibleMove_printBoardPiecesColors() {
+        Board board = new Board(4, 1);
+
+        ChessPiece[][] pieces =
+                new ChessPiece[][] {
+                    {new Rook(board, Color.BLACK)}, {null}, {null}, {new Rook(board, Color.WHITE)}
+                };
+
+        String stringExpected =
+                String.format(
+                        "8 %sR%s %n7 -%s %n6 -%s %n5 %sR%s %n  a b c d e f g h%n",
+                        UI.ANSI_YELLOW,
+                        UI.ANSI_RESET,
+                        UI.ANSI_RESET,
+                        UI.ANSI_RESET,
+                        UI.ANSI_WHITE,
+                        UI.ANSI_RESET);
+
+        ui.printBoard(pieces, null);
+        assertEquals(stringExpected, outputStreamCaptor.toString());
+    }
+
+    @Test
+    @DisplayName("should print the board, pieces, its corresponding colors and possibleMoves")
+    void printBoard_hasPossibleMoves_printBoardPiecesColorsMoves() {
+        Board board = new Board(4, 1);
+
+        ChessPiece[][] pieces =
+                new ChessPiece[][] {
+                    {new Rook(board, Color.BLACK)}, {null}, {null}, {new Rook(board, Color.WHITE)}
+                };
+
+        boolean[][] possibleMoves = new boolean[][] {{true}, {true}, {false}, {false}};
+
+        String stringExpected =
+                String.format(
+                        "8 %s%sR%s %n7 %s-%s %n6 -%s %n5 %sR%s %n  a b c d e f g h%n",
+                        UI.ANSI_BLUE_BACKGROUND,
+                        UI.ANSI_YELLOW,
+                        UI.ANSI_RESET,
+                        UI.ANSI_BLUE_BACKGROUND,
+                        UI.ANSI_RESET,
+                        UI.ANSI_RESET,
+                        UI.ANSI_WHITE,
+                        UI.ANSI_RESET);
+
+        ui.printBoard(pieces, possibleMoves);
+        assertEquals(stringExpected, outputStreamCaptor.toString());
     }
 }
