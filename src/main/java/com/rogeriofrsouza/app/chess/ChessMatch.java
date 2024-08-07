0fbda6f8a20a3c49562939eb1e075f6fbe4e9e3a
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-08-06 22:27:56 -0300

    feat(ChessMatch): improve initialSetup() readability and performance

--- src/main/java/com/rogeriofrsouza/app/chess/ChessMatch.java
+++ src/main/java/com/rogeriofrsouza/app/chess/ChessMatch.java
@@ -42,44 +42,34 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('b', 8, new Knight(board, Color.BLACK));
-        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
-        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-        placeNewPiece('e', 8, new King(board, Color.BLACK, this));
-        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
-        placeNewPiece('g', 8, new Knight(board, Color.BLACK));
-        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('a', 7, new Pawn(board, Color.BLACK, this));
-        placeNewPiece('b', 7, new Pawn(board, Color.BLACK, this));
-        placeNewPiece('c', 7, new Pawn(board, Color.BLACK, this));
-        placeNewPiece('d', 7, new Pawn(board, Color.BLACK, this));
-        placeNewPiece('e', 7, new Pawn(board, Color.BLACK, this));
-        placeNewPiece('f', 7, new Pawn(board, Color.BLACK, this));
-        placeNewPiece('g', 7, new Pawn(board, Color.BLACK, this));
-        placeNewPiece('h', 7, new Pawn(board, Color.BLACK, this));
+        Color color = Color.BLACK;
 
-        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('b', 1, new Knight(board, Color.WHITE));
-        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
-        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
-        placeNewPiece('e', 1, new King(board, Color.WHITE, this));
-        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
-        placeNewPiece('g', 1, new Knight(board, Color.WHITE));
-        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('a', 2, new Pawn(board, Color.WHITE, this));
-        placeNewPiece('b', 2, new Pawn(board, Color.WHITE, this));
-        placeNewPiece('c', 2, new Pawn(board, Color.WHITE, this));
-        placeNewPiece('d', 2, new Pawn(board, Color.WHITE, this));
-        placeNewPiece('e', 2, new Pawn(board, Color.WHITE, this));
-        placeNewPiece('f', 2, new Pawn(board, Color.WHITE, this));
-        placeNewPiece('g', 2, new Pawn(board, Color.WHITE, this));
-        placeNewPiece('h', 2, new Pawn(board, Color.WHITE, this));
+        for (int row : new int[] {8, 7, 2, 1}) {
+            if (row <= 2) {
+                color = Color.WHITE;
+            }
+
+            for (char column = 'a'; column < 'i'; column++) {
+                ChessPiece piece =
+                        (row == 7 || row == 2)
+                                ? new Pawn(board, color, this)
+                                : switchPiece(column, board, color);
+
+                board.placePiece(piece, new ChessPosition(column, row).toPosition());
+                piecesOnTheBoard.add(piece);
+            }
+        }
     }
 
-    private void placeNewPiece(char column, int row, ChessPiece piece) {
-        board.placePiece(piece, new ChessPosition(column, row).toPosition());
-        piecesOnTheBoard.add(piece);
+    private ChessPiece switchPiece(char column, Board board, Color color) {
+        return switch (column) {
+            case 'a', 'h' -> new Rook(board, color);
+            case 'b', 'g' -> new Knight(board, color);
+            case 'c', 'f' -> new Bishop(board, color);
+            case 'd' -> new Queen(board, color);
+            case 'e' -> new King(board, color, this);
+            default -> throw new IllegalStateException("Column not supported.");
+        };
     }
 
     public ChessPiece[][] getPieces() {
