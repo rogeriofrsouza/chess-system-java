f3662a89c06caee593cbb952c5525b5ca17e03c7
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-08-10 00:51:57 -0300

    refactor: rename local variable

--- src/main/java/com/rogeriofrsouza/app/chess/ChessMatch.java
+++ src/main/java/com/rogeriofrsouza/app/chess/ChessMatch.java
@@ -73,15 +73,15 @@ public class ChessMatch {
     }
 
     public ChessPiece[][] getPieces() {
-        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        ChessPiece[][] pieces = new ChessPiece[board.getRows()][board.getColumns()];
 
         for (int i = 0; i < board.getRows(); i++) {
             for (int j = 0; j < board.getColumns(); j++) {
-                mat[i][j] = (ChessPiece) board.piece(i, j);
+                pieces[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
 
-        return mat;
+        return pieces;
     }
 
     public boolean[][] computePossibleMoves(ChessPosition sourcePosition) {
