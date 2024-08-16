0ae7577ea7cf37d7011298e97c6bdadb3f08090f
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-08-16 00:25:08 -0300

    feat(Position): add lombok annotations and remove boilerplate

--- src/main/java/com/rogeriofrsouza/app/boardgame/Position.java
+++ src/main/java/com/rogeriofrsouza/app/boardgame/Position.java
@@ -1,38 +1,17 @@
 package com.rogeriofrsouza.app.boardgame;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Position {
 
     private int row;
     private int column;
 
-    public Position(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
-    public void setColumn(int column) {
-        this.column = column;
-    }
-
     public void setValues(int row, int column) {
         this.row = row;
         this.column = column;
-    }
-
-    @Override
-    public String toString() {
-        return row + ", " + column;
     }
 }
