cc5a4352135ff182fa33f2a074aeb90135ba72c4
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-08-17 16:23:26 -0300

    test: add unit tests for replacePromotedPiece method

--- src/test/java/com/rogeriofrsouza/app/chess/ChessMatchTest.java
+++ src/test/java/com/rogeriofrsouza/app/chess/ChessMatchTest.java
@@ -2,7 +2,6 @@ package com.rogeriofrsouza.app.chess;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
@@ -22,6 +21,7 @@ import com.rogeriofrsouza.app.boardgame.Piece;
 import com.rogeriofrsouza.app.boardgame.Position;
 import com.rogeriofrsouza.app.chess.pieces.Bishop;
 import com.rogeriofrsouza.app.chess.pieces.Knight;
+import com.rogeriofrsouza.app.chess.pieces.Pawn;
 import com.rogeriofrsouza.app.chess.pieces.Queen;
 import com.rogeriofrsouza.app.chess.pieces.Rook;
 
@@ -119,5 +119,52 @@ class ChessMatchTest {
         assertThrowsExactly(
                 ChessException.class,
                 () -> chessMatchMock.computePossibleMoves(chessPosition));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"B", "N", "R", "Q"})
+    @DisplayName("should return the new piece if promoted piece is valid")
+    void replacePromotedPiece_promotedPieceIsValid_returnNewPiece(String type) {
+        chessMatchMock.setPromoted(chessPieceMock);
+
+        ChessPosition chessPosition = new ChessPosition('a', 1);
+        Position promotedPosition = chessPosition.toPosition();
+
+        ChessPiece newPiece =
+                switch (type) {
+                    case "B" -> new Bishop(boardMock, Color.WHITE);
+                    case "N" -> new Knight(boardMock, Color.WHITE);
+                    case "R" -> new Rook(boardMock, Color.WHITE);
+                    default -> new Queen(boardMock, Color.WHITE);
+                };
+
+        when(chessPieceMock.getChessPosition()).thenReturn(chessPosition);
+        when(boardMock.removePiece(promotedPosition)).thenReturn(pieceMock);
+        when(chessPieceMock.getColor()).thenReturn(Color.WHITE);
+        doNothing().when(boardMock).placePiece(newPiece, promotedPosition);
+
+        assertEquals(newPiece, chessMatchMock.replacePromotedPiece(type));
+
+        verify(chessPieceMock).getChessPosition();
+        verify(boardMock).removePiece(promotedPosition);
+        verify(chessPieceMock).getColor();
+        verify(boardMock).placePiece(newPiece, promotedPosition);
+    }
+
+    @Test
+    @DisplayName("should return the current promoted piece if type isn't valid")
+    void replacePromotedPiece_typeNonValid_returnPromotedPiece() {
+        Pawn promoted = new Pawn(boardMock, Color.BLACK, chessMatchMock);
+        chessMatchMock.setPromoted(promoted);
+
+        assertEquals(promoted, chessMatchMock.replacePromotedPiece("A"));
+    }
+
+    @Test
+    @DisplayName("should throw IllegalStateException, no piece to be promoted")
+    void replacePromotedPiece_typeNonValid_throwIllegalStateException() {
+        assertThrowsExactly(
+                IllegalStateException.class,
+                () -> chessMatchMock.replacePromotedPiece("A"));
     }
 }
