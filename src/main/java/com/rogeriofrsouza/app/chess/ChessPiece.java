9551ce6fe35a57d24b0819831a8b0ef1670587ce
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-09-13 21:12:30 -0300

    feat(enum): add inner enum Name in ChessPiece

--- src/main/java/com/rogeriofrsouza/app/chess/ChessPiece.java
+++ src/main/java/com/rogeriofrsouza/app/chess/ChessPiece.java
@@ -4,11 +4,14 @@ import com.rogeriofrsouza.app.boardgame.Board;
 import com.rogeriofrsouza.app.boardgame.Piece;
 import com.rogeriofrsouza.app.boardgame.Position;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 @EqualsAndHashCode(callSuper = true)
 public abstract class ChessPiece extends Piece {
 
+    private Name name;
     private Color color;
     private int moveCount;
 
@@ -43,5 +46,19 @@ public abstract class ChessPiece extends Piece {
 
     protected void decreaseMoveCount() {
         moveCount--;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public enum Name {
+
+        ROOK("R"),
+        KNIGHT("N"),
+        BISHOP("B"),
+        QUEEN("Q"),
+        KING("K"),
+        PAWN("P");
+
+        private String letter;
     }
 }
