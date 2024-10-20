6501f9b22a784ede71900c0df50a182262dd4445
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-10-20 16:39:57 -0300

    refactor(Bishop): pull members up to ChessPiece

--- src/main/java/com/rogeriofrsouza/app/chess/ChessPiece.java
+++ src/main/java/com/rogeriofrsouza/app/chess/ChessPiece.java
@@ -50,6 +50,44 @@ public abstract class ChessPiece extends Piece {
         return getName().getLetter();
     }
 
+    protected void checkMoves(boolean[][] possibleMoves, ChessMoveDirection direction) {
+        Position targetPosition = new Position(position.getRow(), position.getColumn());
+
+        while (true) {
+            changeTargetPosition(targetPosition, direction);
+
+            if (!getBoard().positionExists(targetPosition)) {
+                return;
+            }
+
+            if (getBoard().thereIsAPiece(targetPosition)) {
+                possibleMoves[targetPosition.getRow()][targetPosition.getColumn()] =
+                    isThereOpponentPiece(targetPosition);
+                return;
+            }
+
+            possibleMoves[targetPosition.getRow()][targetPosition.getColumn()] = true;
+        }
+    }
+
+    private void changeTargetPosition(Position targetPosition, ChessMoveDirection direction) {
+        switch (direction) {
+            case UP -> targetPosition.setRow(targetPosition.getRow() - 1);
+            case DOWN -> targetPosition.setRow(targetPosition.getRow() + 1);
+            case LEFT -> targetPosition.setColumn(targetPosition.getColumn() - 1);
+            case RIGHT -> targetPosition.setColumn(targetPosition.getColumn() + 1);
+            case UP_LEFT -> targetPosition.setValues(
+                targetPosition.getRow() - 1, targetPosition.getColumn() - 1);
+            case UP_RIGHT -> targetPosition.setValues(
+                targetPosition.getRow() - 1, targetPosition.getColumn() + 1);
+            case DOWN_LEFT -> targetPosition.setValues(
+                targetPosition.getRow() + 1, targetPosition.getColumn() - 1);
+            case DOWN_RIGHT -> targetPosition.setValues(
+                targetPosition.getRow() + 1, targetPosition.getColumn() + 1);
+            default -> throw new IllegalArgumentException("Invalid direction");
+        }
+    }
+
     @AllArgsConstructor
     @Getter
     public enum Name {
--- src/main/java/com/rogeriofrsouza/app/chess/pieces/Bishop.java
+++ src/main/java/com/rogeriofrsouza/app/chess/pieces/Bishop.java
@@ -1,7 +1,6 @@
 package com.rogeriofrsouza.app.chess.pieces;
 
 import com.rogeriofrsouza.app.boardgame.Board;
-import com.rogeriofrsouza.app.boardgame.Position;
 import com.rogeriofrsouza.app.chess.ChessMoveDirection;
 import com.rogeriofrsouza.app.chess.ChessPiece;
 
@@ -21,43 +20,5 @@ public class Bishop extends ChessPiece {
         checkMoves(possibleMoves, ChessMoveDirection.DOWN_RIGHT);
 
         return possibleMoves;
-    }
-
-    private void checkMoves(boolean[][] possibleMoves, ChessMoveDirection direction) {
-        Position targetPosition = new Position(position.getRow(), position.getColumn());
-
-        while (true) {
-            changeTargetPosition(targetPosition, direction);
-
-            if (!getBoard().positionExists(targetPosition)) {
-                return;
-            }
-
-            if (getBoard().thereIsAPiece(targetPosition)) {
-                possibleMoves[targetPosition.getRow()][targetPosition.getColumn()] =
-                    isThereOpponentPiece(targetPosition);
-                return;
-            }
-
-            possibleMoves[targetPosition.getRow()][targetPosition.getColumn()] = true;
-        }
-    }
-
-    private void changeTargetPosition(Position targetPosition, ChessMoveDirection direction) {
-        switch (direction) {
-            case UP -> targetPosition.setRow(targetPosition.getRow() - 1);
-            case DOWN -> targetPosition.setRow(targetPosition.getRow() + 1);
-            case LEFT -> targetPosition.setColumn(targetPosition.getColumn() - 1);
-            case RIGHT -> targetPosition.setColumn(targetPosition.getColumn() + 1);
-            case UP_LEFT -> targetPosition.setValues(
-                targetPosition.getRow() - 1, targetPosition.getColumn() - 1);
-            case UP_RIGHT -> targetPosition.setValues(
-                targetPosition.getRow() - 1, targetPosition.getColumn() + 1);
-            case DOWN_LEFT -> targetPosition.setValues(
-                targetPosition.getRow() + 1, targetPosition.getColumn() - 1);
-            case DOWN_RIGHT -> targetPosition.setValues(
-                targetPosition.getRow() + 1, targetPosition.getColumn() + 1);
-            default -> throw new IllegalArgumentException("Invalid direction");
-        }
     }
 }
