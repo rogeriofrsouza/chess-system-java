481341afe8ed52660740f71af124644025ff8040
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2025-09-06 15:20:07 -0300

    refactor: remove serial from exceptions

--- src/main/java/com/rogeriofrsouza/app/boardgame/BoardException.java
+++ src/main/java/com/rogeriofrsouza/app/boardgame/BoardException.java
@@ -1,11 +1,6 @@
 package com.rogeriofrsouza.app.boardgame;
 
-import java.io.Serial;
-
 public class BoardException extends RuntimeException {
-
-    @Serial
-    private static final long serialVersionUID = 1L;
 
     public BoardException(String msg) {
         super(msg);
--- src/main/java/com/rogeriofrsouza/app/chess/ChessException.java
+++ src/main/java/com/rogeriofrsouza/app/chess/ChessException.java
@@ -1,13 +1,8 @@
 package com.rogeriofrsouza.app.chess;
 
-import java.io.Serial;
-
 import com.rogeriofrsouza.app.boardgame.BoardException;
 
 public class ChessException extends BoardException {
-
-    @Serial
-    private static final long serialVersionUID = 1L;
 
     public ChessException(String msg) {
         super(msg);
