5cf1e6445e53420f1918245573d684cac0227255
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2025-10-15 21:03:45 -0300

    refactor(chess): convert to record class

--- src/main/java/com/rogeriofrsouza/app/chess/ChessPosition.java
+++ src/main/java/com/rogeriofrsouza/app/chess/ChessPosition.java
@@ -1,29 +1,16 @@
 package com.rogeriofrsouza.app.chess;
 
 import com.rogeriofrsouza.app.boardgame.Position;
-import lombok.Getter;
 
-@Getter
-public class ChessPosition {
+public record ChessPosition(char column, int row) {
 
-    private final char column;
-    private final int row;
-
-    public ChessPosition(char column, int row) {
+    public ChessPosition {
         if (column < 'a' || column > 'h' || row < 1 || row > 8) {
             throw new IllegalArgumentException("Invalid chess position. Should be a1 - h8");
         }
-
-        this.column = column;
-        this.row = row;
     }
 
     public Position toPosition() {
         return new Position(8 - row, column - 'a');
-    }
-
-    @Override
-    public String toString() {
-        return "" + column + row;
     }
 }
